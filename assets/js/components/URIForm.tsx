186a2d227bacce3540b7c4690bd1de4c40daa2ff
Joshua Lee <josh@joshuamlee.com>
2022-03-21 18:00:11 -0400

    Adds error handling to front end

--- assets/js/components/URIForm.tsx
+++ assets/js/components/URIForm.tsx
@@ -1,11 +1,58 @@
 import React, { useState } from 'react';
 
-export default function URIForm() {
-  const [values, setValues] = useState({
-    uri: '',
-  });
+const SERVER_URL = 'http://localhost:8080';
 
-  const handleInputChange = (event: React.FormEvent<HTMLFormElement>) => {
+export default function URIForm() {
+  const initialValues = {
+    uri: '',
+  }
+
+  const [errors, setErrors] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
+  const [shortcuts, setShortcuts] = useState([]);
+  const [values, setValues] = useState(initialValues);
+
+  const callApi = async (uri: string) => {
+    try {
+      let response = await fetch(SERVER_URL + '/api/uris', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'shortened_uri': {
+            uri
+          }
+        }),
+      })
+      let responseBody = await response.json();
+      if (response.status === 201) {
+        resetForm()
+        setShortcuts((shortcut) => ([...shortcuts, responseBody.data]))
+      } else if (response.status === 422) {
+        setErrors((errors) => ([...errors, 'Your URL format is invalid']))
+      } else {
+        setErrors((errors) => ([...errors, 'Server Error']))
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const ensureURIProtocol = (uri: string) => {
+    return (uri.match(/^https?:\/\//))
+      ? uri
+      : 'http://' + uri
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setSubmitted(true);
+    await callApi(ensureURIProtocol(values.uri));
+    setSubmitted(false);
+  };
+
+  const handleTextInputChange = (event: React.FormEvent<HTMLFormElement>) => {
     event.persist()
     setValues((values) => ({
       ...values,
@@ -13,43 +60,63 @@ export default function URIForm() {
     }))
   }
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log(data);
-  };
+  const resetForm = () => {
+    setValues(initialValues);
+  }
 
   return (
-    <section className="section is-large">
-      <div className="container">
-        <h1 className="title">Lynx: The URL Shortener</h1>
-        <h2 className="subtitle"><em>a wild cat with yellowish-brown fur (sometimes spotted), a short tail, and tufted ears, found chiefly in the northern latitudes of North America and Eurasia.</em></h2>
-        <form onSubmit={handleSubmit}>
-          <div className="field">
-            <label className="label">Enter a URL to shorten:</label>
-          </div>
-          <div className="field has-addons">
-            <p className="control">
-              <a className="button is-static is-large">
-                https://
+    <div>
+      <section className="section is-small">
+        <div className="container">
+          <h1 className="title">Lynx: The URL Shortener</h1>
+          <h2 className="subtitle"><em>a wild cat with yellowish-brown fur (sometimes spotted), a short tail, and tufted ears, found chiefly in the northern latitudes of North America and Eurasia.</em></h2>
+          {errors.map(error =>
+            <div className="notification is-danger"><p>{error}</p></div>
+          )}
+          <form onSubmit={handleSubmit}>
+            <div className="field">
+              <label className="label">Enter a URL to shorten:</label>
+            </div>
+            <div className="field has-addons">
+              <p className="control">
+                <a className="button is-static is-large">
+                  https://
+                </a>
+              </p>
+              <div className="control">
+                <input
+                  className="input is-large"
+                  name="uri"
+                  type="text"
+                  placeholder="www.joshuamlee.com"
+                  value={values.uri}
+                  onChange={handleTextInputChange}
+                />
+              </div>
+              <div className="control">
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="button is-info is-large"
+                  disabled={submitted}
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+      <section className="section is-small">
+        <h2 className="subtitle">Your shortcuts:</h2>
+        <ul>
+          {shortcuts.map(shortcut =>
+            <li key={shortcut.id}>
+              <a href={SERVER_URL + "/" + shortcut.slug}>
+                {SERVER_URL + "/" + shortcut.slug} -> {shortcut.uri}
               </a>
-            </p>
-            <div className="control">
-              <input
-                className="input is-large"
-                name="uri"
-                type="text"
-                placeholder="www.joshuamlee.com"
-                value={values.uri}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="control">
-              <input type="submit" name="submit" value="Submit" className="button is-primary is-large" />
-            </div>
-          </div>
-        </form>
-      </div>
-    </section>
+            </li>
+          )}
+        </ul>
+      </section>
+    </div>
   );
 }
